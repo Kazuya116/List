43e60d063f9d1c28647ef757436ed32a2a08d69e
Kazuya116 <amanpratapsingh116@gmail.com>
2024-02-08 23:01:30 +0530

    create the circular linked list using Doubly Linked list.

---
+++ src/main/java/Cicular/IntegerCicularList.java
@@ -0,0 +1,57 @@
+package Cicular;
+import Doubly_Linked_List.Node.*;
+import Doubly_Linked_List.List.IntegerLinkedList;
+
+public class IntegerCicularList extends IntegerLinkedList {
+            public void makeCircular() {
+                if (head != null) {
+                    tail.nextnode = head;
+                    head.prevnode = tail;
+                }
+            }
+
+                @Override
+                public void addFirst(int data){
+                    super.addFirst(data);
+                    makeCircular();
+                }
+
+                @Override
+                public void add(int data){
+                    super.add(data);
+                    makeCircular();
+                }
+                @Override
+
+                public int remove(int data){
+                    int value = super.remove(data);
+                    return value ;
+                }
+
+                public void print(){
+                    Node current = head ;
+                    if(current != null){
+                        do{
+                            System.out.print(current.data + "<<-->>");
+                            current = current.nextnode;
+                        } while(current != head );
+                        System.out.print("NULL");
+                    }
+                }
+
+                public Node search(int data){
+                    Node current = head;
+                    if(current != null){
+                        do{
+                            if(current.data  == data) return current;
+                            current = current.nextnode;
+                        }while(current != head);
+                    }
+                    return current;
+                }
+            }
+
+
+
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import Cicular.IntegerCicularList;
+
+public class Main {
+    public static void main(String[] args) {
+        var circular = new IntegerCicularList();
+        circular.add(100);
+        circular.print();
+    }
+}
